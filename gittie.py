714b441e73b950a2eb2e979fdf5829d41e15154e
Ricricric <nador.attila@gmail.com>
2018-03-27 14:34:48 +0200

    added sg

--- gittie.py
+++ gittie.py
@@ -1,4 +1,14 @@
 def hello():
-    print("Hello world!")
+    print("Hello world!\n")
 
 hello()
+
+def joke():
+    print("Why do fish swim?\n")
+    answer = input("Do you know the answer?(y/n) : ")
+    if answer == "n":
+        print("Because their legs dont reach the bottom!")
+    else:
+        print("Damn it")
+
+joke()
